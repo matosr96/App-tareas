f70de61dd3b8594b8e3bb61dc351b3fe006dac1a
matosr96 <19matos96@gmail.com>
2022-07-20 10:42:09 -0500

    update

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -3,7 +3,7 @@ import { pool } from "../db.js";
 export const getTasks = async (req, res) => {
   try {
     const [result] = await pool.query(
-      "SELECT * FROM tasks ORDER BY createAt ASC"
+      "SELECT * FROM tasks ORDER BY done"
     );
     res.json(result);
   } catch (error) {
