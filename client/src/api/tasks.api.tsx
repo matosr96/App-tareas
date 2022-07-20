2b1b88e544d1f9d4292cbace68a45df4483e07f7
matosr96 <19matos96@gmail.com>
2022-07-20 10:38:35 -0500

    update

--- client/src/api/tasks.api.tsx
+++ client/src/api/tasks.api.tsx
@@ -1,9 +1,13 @@
 import axios from "axios";
-import Axios from "axios";
 
 export const getTaskRequest = async () =>
   await axios.get("http://localhost:4000/tasks");
 
-export const createTaskRequest = async (props: any) => {
+export const createTaskRequest = async (props: any) =>
   await axios.post("http://localhost:4000/tasks", props);
-};
+
+export const updateTaskRequest = async (id: number, props: any) =>
+  await axios.put(`http://localhost:4000/tasks/${id}`, props);
+
+export const deleteTaskRequest = async (id: number) =>
+  await axios.delete(`http://localhost:4000/tasks/${id}`);
--- client/src/screens/Task.tsx
+++ client/src/screens/Task.tsx
@@ -1,15 +1,24 @@
 import { useEffect, useState } from "react";
-import { createTaskRequest, getTaskRequest } from "../api/tasks.api";
+import {
+  createTaskRequest,
+  deleteTaskRequest,
+  getTaskRequest,
+  updateTaskRequest,
+} from "../api/tasks.api";
 import "../styles/tasks.css";
 
 const Task = () => {
   const [tasks, setTasks] = useState([]);
   const [openModal, setOpenModal] = useState(false);
+  const [openModalUpdate, setOpenModalUpdate] = useState(false);
+
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
-  const [openMenu, setOpenMenu] = useState(false);
-  const [item, setItem] = useState("");
+  const [titleUpdate, setTitleUpdate] = useState("");
+  const [descriptionUpdate, setDescriptionUpdate] = useState("");
+  const [UpdateId, setUpdateId] = useState(0);
+
 
   const submitCreateHandler = (e: any) => {
     e.preventDefault();
@@ -17,6 +26,42 @@ const Task = () => {
       title: title,
       description: description,
     });
+    setOpenModal(false);
+    window.location.reload();
+  };
+
+  //UPDATE PROCCES
+  const updateHandler = async (
+    id: number,
+    title: string,
+    description: string
+  ): Promise<void> => {
+    setUpdateId(id);
+    setTitleUpdate(title);
+    setDescriptionUpdate(description);
+    console.log("DATOS", id, title, description);
+
+    await setOpenModalUpdate(!openModalUpdate);
+  };
+
+  const submitUpdateHandler = (e: any): void => {
+    e.preventDefault();
+    setOpenModalUpdate(!openModalUpdate);
+    updateTaskRequest(UpdateId, {
+      title: titleUpdate,
+      description: descriptionUpdate,
+    });
+    window.location.reload();
+  };
+
+  const submitUpdateStateHandler = (id: number, done: boolean): void => {
+    updateTaskRequest(id, { done: !done });
+    window.location.reload();
+  };
+
+  const deleteHandler = (id: number) => {
+    deleteTaskRequest(id);
+    window.location.reload();
   };
 
   useEffect(() => {
@@ -24,12 +69,9 @@ const Task = () => {
       const response = await getTaskRequest();
       const { data } = response;
       setTasks(data);
-      console.log("RTA", data);
     }
     loadTask();
   }, []);
-
-  console.log(tasks);
 
   return (
     <>
@@ -50,25 +92,53 @@ const Task = () => {
           <table>
             <thead>
               <tr>
+                <th>Estado</th>
                 <th>Id</th>
                 <th>Titulo</th>
                 <th className="desc">Descripcion</th>
+
                 <th>Actions</th>
               </tr>
             </thead>
             <tbody>
               {tasks?.map((e: any) => (
                 <tr key={e.id}>
+                  {!e.done ? (
+                    <td>
+                      <button
+                        className="state-task"
+                        onClick={() => submitUpdateStateHandler(e.id, e.done)}
+                      >
+                        <i className="bx bxs-hourglass-top"></i> Pendiente
+                      </button>
+                    </td>
+                  ) : (
+                    <td>
+                      <div className="realizada">
+                        <i className="bx bxs-check-square"></i>
+                      </div>
+                    </td>
+                  )}
                   <td>{e.id}</td>
                   <td>{e.title}</td>
                   <td className="desc">{e.description}</td>
+
                   <td>
                     <div className="actions">
-                      <button className="btn-actions edit">
+                      <button
+                        className="btn-actions edit"
+                        onClick={() => {
+                          updateHandler(e.id, e.title, e.description);
+                          setOpenModalUpdate(!openModalUpdate);
+                        }}
+                      >
                         <i className="bx bx-edit"></i>
                       </button>
-                      <button className="btn-actions trash">
-                      <i className='bx bx-trash' ></i>
+                      <button
+                        className="btn-actions trash"
+                        onClick={() => deleteHandler(e.id)}
+                      >
+                        <i className="bx bx-trash"></i>
                       </button>
                     </div>
                   </td>
@@ -114,6 +184,47 @@ const Task = () => {
           </form>
           <div className="modal_footer">
             <button className="btn" onClick={submitCreateHandler}>
+              <h2>Guardar</h2>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className={openModalUpdate ? "openModal" : "closeModal"}>
+        <div className="modal">
+          <div className="modal_header">
+            <h2 className="titlemodal">Editar Tarea</h2>
+            <button
+              className="modal-close"
+              onClick={() => setOpenModalUpdate(!openModalUpdate)}
+            >
+              <i className="bx bx-x"></i>
+            </button>
+          </div>
+
+          <form action="" className="form_items">
+            <div className="input">
+              <label htmlFor="">Titulo de la tarea</label>
+              <input
+                type="text"
+                placeholder=""
+                value={titleUpdate}
+                onChange={(e) => setTitleUpdate(e.target.value)}
+              />
+            </div>
+
+            <div className="input">
+              <label htmlFor="">Descripcion de la tarea</label>
+              <textarea
+                placeholder=""
+                value={descriptionUpdate}
+                onChange={(e) => setDescriptionUpdate(e.target.value)}
+                rows={5}
+              ></textarea>
+            </div>
+          </form>
+          <div className="modal_footer">
+            <button className="btn" onClick={submitUpdateHandler}>
               <h2>Guardar</h2>
             </button>
           </div>
